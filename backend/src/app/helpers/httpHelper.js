2bb99dfdf528daedfc68feda8334e67cdb2f5524
Everson Silva <eversonsilva9799@gmail.com>
2020-07-21 08:18:52 -0300

    Add helpers to http

---
+++ backend/src/app/helpers/httpHelper.js
@@ -0,0 +1,28 @@
+const ServerError = require('../errors/ServerError');
+
+const badRequest = (error) => {
+    return {
+        statusCode: 400,
+        error: error
+    }
+}
+
+const serverError = () => {
+    return {
+        statusCode: 500,
+        error: new ServerError()
+    }
+}
+
+const ok = (data) => {
+    return {
+        statusCode: 200,
+        body: data
+    }
+}
+
+module.exports = {
+    badRequest,
+    serverError,
+    ok
+}
